0e109e7c1d80856b22aeb4295abee4f56896d05c
Konin-Sensei <dmsconway@gmail.com>
2019-06-11 08:29:51 -0400

    UI scaling improvements

---
+++ Assets/Scripts/QuitDoor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitDoor : MonoBehaviour {
+
+	void OnTriggerStay2D (Collider2D other)
+	{
+		if (other.tag == "Player" && Input.GetKeyDown(KeyCode.F)) {
+			Application.Quit();
+		}
+	}		
+}
