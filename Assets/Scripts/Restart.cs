21cf74f2ad01226db42f88f35902f7f3fd56aba5
Paul Noujaim <boulosnoujaim@gmail.com>
2019-06-11 14:54:50 -0400

    Changed Restart Script
    Restart script altered to no longer requires the level to be specified. Can now be used immediately after applying the script

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -6,10 +6,18 @@ using UnityEngine.SceneManagement;
 public class Restart : MonoBehaviour {
 	
 	// Update is called once per frame
+
+	string scene;
+
+	void Start () {
+		scene = SceneManager.GetActiveScene().name;
+
+
+	}
 	void Update () {
 
 		if (Input.GetKeyDown (KeyCode.R)) {
-			SceneManager.LoadScene ("Level One");
+			SceneManager.LoadScene (scene);
 		}
 		
 	}
