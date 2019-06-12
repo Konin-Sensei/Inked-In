498740401b6c10cb48368b8d1c2638110d17bf40
franciscosempereperez <kittymagdalenas@gmail.com>
2019-06-12 15:26:49 -0400

    began the next step of making the enemy see platform boundaries (raycasting) but taking a break to focus on menus and saving.

--- Assets/RicksTest/Mover.cs
+++ Assets/RicksTest/Mover.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+	//stuff that's implemented
 	private Transform target;
 	public Vector2 push;
 	public Rigidbody2D rb;
-	public BoxCollider2D bc;
+	public BoxCollider2D largeCollider;
+	
+	//stuff that's not implemented yet.
+	public Transform currentGround;
+	public RaycastHit2D[] raycastResults; //what the raycast "saw"
+	public ContactFilter2D contactFilter; //filters results?
 	
     // Start is called before the first frame update
     void Awake()
     {
 		rb = gameObject.GetComponent<Rigidbody2D>();
-		bc = gameObject.GetComponent<BoxCollider2D>();
+		largeCollider = gameObject.GetComponent<BoxCollider2D>();
+			//NEXT STEPS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			//set up a raycast pointing down that should return the platform in raycastResults
+			//and use that reference in order to set boundaries on where the enemy can walk.
+			//Long term goal: 
 		target = GameObject.FindGameObjectWithTag ("Player").transform;
         
     }
@@ -48,11 +58,11 @@ public class Mover : MonoBehaviour
 		Vector2 push = new Vector2(0, 0);
 		if(movex > 0f)
 		{
-			push = new Vector2(100f, 0f);
+			push = new Vector2(10f, 0f);
 		}
 		else if(movex < 0f)
 		{
-			push = new Vector2(-100f, 0f);
+			push = new Vector2(-10f, 0f);
 		}
 		
 		rb.AddForce(push);
