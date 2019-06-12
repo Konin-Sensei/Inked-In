77363ebce40f147ae93eede56a2ac8be7d113f61
Konin-Sensei <dmsconway@gmail.com>
2019-06-12 15:48:25 -0400

    Lots o' Shit
    Resets on falling off the map.
    Camera swaps in tutorial.

---
+++ Assets/Scripts/Camera_Trigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Trigger : MonoBehaviour
+{
+    public Swap_Cameras swap_script;
+
+    void OnTriggerStay2D(Collider2D other){
+        if(other.tag == "Player"){
+            swap_script.enable_gap_camera();
+        }
+    }
+    void OnTriggerExit2D(Collider2D other){
+        if(other.tag=="Player"){
+            swap_script.enable_follow_camera();
+        }
+    }
+}
---
+++ Assets/Scripts/Swap_Cameras.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swap_Cameras : MonoBehaviour
+{
+    public Camera follow_cam;
+    public Camera gap_cam;
+
+    void Start(){
+        follow_cam.gameObject.SetActive(true);
+        follow_cam.gameObject.tag = "MainCamera";
+        gap_cam.gameObject.SetActive(false);
+        gap_cam.gameObject.tag = "Inactive Camera";
+    }
+
+    public void enable_follow_camera(){
+        gap_cam.gameObject.SetActive(false);
+        gap_cam.gameObject.tag = "Inactive Camera";
+        follow_cam.gameObject.SetActive(true);
+        follow_cam.gameObject.tag = "MainCamera";
+    }
+
+    public void enable_gap_camera(){
+        follow_cam.gameObject.SetActive(false);
+        follow_cam.gameObject.tag = "Inactive Camera";
+        gap_cam.gameObject.SetActive(true);
+        gap_cam.gameObject.tag = "MainCamera";
+    }
+}
