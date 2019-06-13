8e8725d04e851d18ee55b5a4481cf54cc661769a
Konin-Sensei <dmsconway@gmail.com>
2019-06-13 07:52:57 -0400

    Revert "Added some save features to the door scripts. Not done yet, but need to commit frequently, senpai says."
    This reverts commit 3bd412bb2ec5bc0cb6384a42815bc2d94d8e3bc7.

--- Assets/Scripts/NextLevelDoor.cs
+++ Assets/Scripts/NextLevelDoor.cs
@@ -6,20 +6,11 @@ using UnityEngine.SceneManagement;
 public class NextLevelDoor : MonoBehaviour {
 
 	public string nextLevel;
-	public bool levelSelectDoor; //this must be set so we can make sure you can't just enter any level in levelSelect menu
-	private SaveDoor screwKonin = new SaveDoor();
 
 	void OnTriggerStay2D (Collider2D other)
 	{
-		if (other.tag == "Player" && Input.GetKeyDown(KeyCode.F) && !levelSelectDoor) {
-			screwKonin.trySave(nextLevel); //we need to make sure the var nextLevel in this is the same as the keys in SaveDoor for this to work...
+		if (other.tag == "Player" && Input.GetKeyDown(KeyCode.F)) {
 			SceneManager.LoadScene (nextLevel);
-		}
-		if(other.tag == "Player" && Input.GetKeyDown(KeyCode.F) && levelSelectDoor) {
-			if(PlayerPrefs.GetInt(nextLevel, 0) == 1)
-			{
-				SceneManager.LoadScene(nextLevel); //if this is on the level select screen, you can only travel if you've beaten the previous level.
-			}
 		}
 	}		
 }
--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 public class OpenDoor : MonoBehaviour
 {
     public SpriteRenderer[] doorSprite;
-	public string nextLevel;
-	public bool alwaysOpen;
 
     void Start(){
         GetComponent<SpriteRenderer>().sprite = doorSprite[0].sprite;
     }
 
     void OnTriggerEnter2D(Collider2D other){
-	if((PlayerPrefs.GetInt(nextLevel, 0) == 1) || alwaysOpen)//The door only opens a crack if you've beaten the level it came from or if we set it to always be open (levelSelect/startgame/quit/etc)
-		{
-			GetComponent<SpriteRenderer>().sprite = doorSprite[1].sprite;
-		}
+        GetComponent<SpriteRenderer>().sprite = doorSprite[1].sprite;
     }
 
     void OnTriggerExit2D(Collider2D other){
