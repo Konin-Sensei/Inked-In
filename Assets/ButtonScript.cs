6875f5bc81e7b16c0541b06ac834aa8e46b92994
Konin-Sensei <dmsconway@gmail.com>
2019-06-19 23:33:19 -0400

    fuck

--- Assets/ButtonScript.cs
+++ Assets/ButtonScript.cs
@@ -8,19 +8,12 @@ public class ButtonScript : MonoBehaviour
     public GameObject button_pressed;
     public GameObject acid_ferry;
     public GameObject elevator;
-    public GameObject elevator_sign;
-    public GameObject ferry_sign1;
-    public GameObject ferry_sign2;
-
 
     void Start(){
         button_pressed.SetActive(false);
         button_unpressed.SetActive(true);
         acid_ferry.SetActive(false);
         elevator.SetActive(false);
-        elevator_sign.SetActive(false);
-        ferry_sign1.SetActive(false);
-        ferry_sign2.SetActive(false);
     }
 
     void OnTriggerEnter2D(Collider2D other){
@@ -29,10 +22,6 @@ public class ButtonScript : MonoBehaviour
             button_unpressed.SetActive(false);
             acid_ferry.SetActive(true);
             elevator.SetActive(true);
-            elevator_sign.SetActive(true);
-            elevator_sign.transform.Rotate(Vector3.forward * -200);
-            ferry_sign1.SetActive(true);
-            ferry_sign2.SetActive(true);
         }
     }
 }
