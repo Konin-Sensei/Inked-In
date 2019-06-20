339b51222b9a29f1ed9ffd05d31e9c4cd4202f4d
Paul Noujaim <boulosnoujaim@gmail.com>
2019-06-19 22:13:38 -0400

    Basedot/bouncing changes

---
+++ Assets/Scripts/BouncingScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncingScript : MonoBehaviour
+{
+
+    public float bounciness;
+    Rigidbody2D myBody;
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        print("Method entered");
+        myBody = other.rigidbody;
+        myBody.AddForce(new Vector2(other.relativeVelocity.x, other.relativeVelocity.y+4)*bounciness, ForceMode2D.Impulse);
+
+    }
+
+}
