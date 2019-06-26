18a272cccbb8672ce122ca88719b5108a16411ff
Konin-Sensei <dmsconway@gmail.com>
2019-06-26 11:59:11 -0400

    reasons

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -6,9 +6,11 @@ using UnityEngine.SceneManagement;
 public class PlayerDeath : MonoBehaviour
 {
     public string level_name;
+    public float level_index;
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Player"){
             SceneManager.LoadScene(level_name);
+            ColorProgress.level[level_index] = false;
         }
     }
 }
