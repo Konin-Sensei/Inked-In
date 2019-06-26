866c3fd4d1a08dc2204b4533062dfc1c15145c80
Konin-Sensei <dmsconway@gmail.com>
2019-06-26 18:29:34 -0400

    stupid

---
+++ Assets/BugsButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BugsButton : MonoBehaviour
+{
+    public GameObject[] doors1 = new GameObject[2];
+    public GameObject[] doors2 = new GameObject[2];
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
