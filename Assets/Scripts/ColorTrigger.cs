4f9c512b20320e246be6f0915b129f51cc563d26
Paul Noujaim <boulosnoujaim@gmail.com>
2019-06-26 18:31:22 -0400

    ree

--- Assets/Scripts/ColorTrigger.cs
+++ Assets/Scripts/ColorTrigger.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class ColorTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void setProgress ()
     {
-        
+        ColorProgress.level[8] = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/volumehandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class volumehandler : MonoBehaviour
+{
+
+    void Start(){
+        Volume.volume_level = 1;
+    }
+    // Start is called before the first frame update
+    void Update(){
+        gameObject.GetComponent<AudioSource>().volume = Volume.volume_level;
+    }
+}
