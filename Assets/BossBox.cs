ea22751ed4122adf0fa08f0aeabe58ed4db6e941
Konin-Sensei <dmsconway@gmail.com>
2019-06-27 05:19:05 -0400

    push

---
+++ Assets/BossBox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBox : MonoBehaviour
+{
+    public Material[] materials = new Material[9];
+    // Start is called before the first frame update
+    void Start(){
+        RenderSettings.skybox = materials[BossProgress.getLevel()];
+    }
+    void Update(){
+        RenderSettings.skybox = materials[BossProgress.getLevel()];
+    }
+}
---
+++ Assets/RockTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockTrigger : MonoBehaviour
+{
+    public GameObject rock;
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Player"){
+            GameObject rock_clone = Instantiate(rock, transform.position, transform.rotation) as GameObject;
+        }
+    }
+}
