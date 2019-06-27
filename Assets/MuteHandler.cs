018a7789c73f9cb82038d0d16cb532ac04ed454d
Paul Noujaim <boulosnoujaim@gmail.com>
2019-06-26 21:26:15 -0400

    Level Select, Menu Settings

--- Assets/MuteHandler.cs
+++ Assets/MuteHandler.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MuteHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+private bool status;
+
+public Button target;
+/* 
+public Image volumeNeutral;
+public Sprite volumeHighlight;
+public Sprite volumePressed;
+public Image mutedNeutral;
+public Sprite mutedHighlight;
+public Sprite mutedPressed;*/
+
+public SpriteState unMuted = new SpriteState();
+public SpriteState muted = new SpriteState();
+public AudioSource audioSource;
+
     void Start()
-    {
-        
+    {   
+        target.spriteState = unMuted;
+        status = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (status)
+        {
+            audioSource.Pause();
+            target.spriteState = muted;
+        } else{
+            audioSource.UnPause();
+            target.spriteState = unMuted;
+        }
+    }
+
+    public void OnMouseDown() 
+    {
+        status = !status;       
     }
 }
