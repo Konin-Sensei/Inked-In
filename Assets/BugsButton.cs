fabe5b6c37214bce5b8029eaad5965f5d08cd03d
Konin-Sensei <dmsconway@gmail.com>
2019-06-26 23:37:42 -0400

    a commit

--- Assets/BugsButton.cs
+++ Assets/BugsButton.cs
@@ -7,13 +7,20 @@ public class BugsButton : MonoBehaviour
 {
     public GameObject[] doors1 = new GameObject[2];
     public GameObject[] doors2 = new GameObject[2];
+
     void Start(){
-        
+        doors1[0].SetActive(true);
+        doors1[1].SetActive(true);
+        doors2[0].SetActive(false);
+        doors2[1].SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Player"){
+            doors1[0].SetActive(false);
+            doors1[1].SetActive(false);
+            doors2[0].SetActive(true);
+            doors2[1].SetActive(true);
+        }
     }
 }
