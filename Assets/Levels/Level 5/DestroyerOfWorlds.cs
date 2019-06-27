01e9d8079b42d83fd7e29ffa760feac6cac9a3e2
Konin-Sensei <dmsconway@gmail.com>
2019-06-27 02:18:59 -0400

    ricky

---
+++ Assets/Levels/Level 5/DestroyerOfWorlds.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerOfWorlds : MonoBehaviour
+{	
+	 void OnTriggerEnter2D(Collider2D other)
+	 {
+		 Destroy(other);
+	 }//the idea was to run from this guy to make the game slightly more interesting. A smarter player could try and trigger the trap on the enemy to end that
+	 //hazard. Issue being the actual implementation of a trap, which I cannot do at this point, mentally.
+	
+}
---
+++ Assets/Levels/Level 5/MovingSpike.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSpike : MonoBehaviour
+{
+  public Rigidbody2D body;
+  public BoxCollider2D trig;
+  
+
+  Vector2 down = new Vector2(0, -1);
+  Vector2 up = new Vector2(0, 1);
+  
+  void OnTriggerEnter2D(Collider2D other)
+  {
+	  getGoing();
+  }
+  
+  
+  void getGoing()
+  {
+	
+	body.velocity = down;
+  }
+}
