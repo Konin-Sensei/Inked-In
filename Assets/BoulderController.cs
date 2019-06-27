9acff1029715d164c45e4d8d0a370167298fc471
Konin-Sensei <dmsconway@gmail.com>
2019-06-27 00:32:33 -0400

    stuff

---
+++ Assets/BoulderController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderController : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start(){
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        rigidbody.velocity = new Vector2(Time.deltaTime * speed, rigidbody.velocity.y);
+    }
+}
